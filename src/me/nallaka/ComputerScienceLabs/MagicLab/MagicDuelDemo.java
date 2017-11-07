af51f605a5349fcb34ae510fc3a81c64b7b30558
Nallaka <mithul0700@gmail.com>
2017-11-06 22:28:46 -0600

    Choosing Spells out of 5 and beginning demo class

--- src/me/nallaka/ComputerScienceLabs/MagicLab/MagicDuelDemo.java
+++ src/me/nallaka/ComputerScienceLabs/MagicLab/MagicDuelDemo.java
@@ -1,7 +1,62 @@
 package me.nallaka.ComputerScienceLabs.MagicLab;
 
+import java.util.Scanner;
+
 public class MagicDuelDemo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int menuChoice;
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("Welcome to the Main Menu. Please choose one of the following options:\n1) Start a New Duel\n2)Quit" );
+            if (scanner.hasNextInt()) {
+                menuChoice = scanner.nextInt();
+            } else {
+                System.out.println("Please use a valid input");
+                continue;
+            }
+            switch (menuChoice) {
+                case 1:
+                    boolean gameQuit = false;
+                    Wizard wizardOne = new Wizard();
+                    Wizard wizardTwo = new Wizard();
+                    while (!gameQuit) {
+                        System.out.println("Welcome to the Game Menu. Please choose one of the following options:\n1) Duel! \n2)Quit");
+                        if (scanner.hasNextInt()) {
+                            menuChoice = scanner.nextInt();
+                        } else {
+                            System.out.println("Please use a valid input");
+                            continue;
+                        }
+                        switch (menuChoice) {
+                            case 1:
+                                System.out.println("Wizard One: Enter your name:");
+                                wizardOne.setName(scanner.nextLine());
+                                System.out.println("Wizard Two: Enter your name:");
+                                wizardTwo.setName(scanner.nextLine());
+                                System.out.println("Wizard One: Choose 3 of the following:\n1) Fireball\n2) Arrow Rain\n3) Thunder Bolt\n4) Blood Leech\n5) Shadow Sword");
+                                wizardOne.setWizardSpells(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+                                System.out.println("Wizard Two: Choose 3 of the following:\n1) Fireball\n2) Arrow Rain\n3) Thunder Bolt\n4) Blood Leech\n5) Shadow Sword");
+                                wizardTwo.setWizardSpells(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+                                while (wizardOne.getHealth() > 0 || wizardTwo.getHealth() > 0) {
+                                    System.out.println("Wizard One: Choose a spell to cast");
+                                    System.out.println(wizardOne.getWizardSpells());
+                                    System.out.println("Wizard Two: Choose a spell to cast");
+                                    System.out.println(wizardTwo.getWizardSpells());
+                                }
+                                break;
+                            case 2:
+                                quit = true;
+                                break;
 
+                        }
+                    }
+                    break;
+                case 2:
+                    quit = true;
+                    break;
+
+            }
+        }
     }
 }
--- src/me/nallaka/ComputerScienceLabs/MagicLab/Wizard.java
+++ src/me/nallaka/ComputerScienceLabs/MagicLab/Wizard.java
@@ -5,33 +5,97 @@ public class Wizard {
     private double health, maxHealth;
     private int score;
 
+    private Spell spellOne;
+    private Spell spellTwo;
+    private Spell spellThree;
+
     private Spell fireBall = new Spell("FireBall", 8, 2, 2, 3);
     private Spell arrowRain = new Spell("Arrow Rain", 6, 2, 3, 2);
     private Spell thunderbolt = new Spell("ThunderBolt", 10, 5, 5, 7);
     private Spell bloodLeech = new Spell("Blood Leech", 10, 5, 4, 6);
     private Spell shadowWeapon = new Spell("Shadow Sword", 9, 3, 3, 3);
 
-    public void castSpell(Spell spell, Wizard wizardCaster, Wizard wizardReciever) {
+
+    public void castSpell(Spell spell, Wizard wizardCaster, Wizard wizardReceiver) {
         double spellDamage = spell.getSpellDamage(spell);
         if (spellDamage != 0) {
-            wizardReciever.setHealth(wizardReciever.getHealth() - spellDamage);
-            System.out.println(wizardCaster.getName() + " hit " + wizardReciever.getName() + " for " + spellDamage + " hp.");
+            wizardReceiver.setHealth(wizardReceiver.getHealth() - spellDamage);
+            System.out.println(wizardCaster.getName() + " hit " + wizardReceiver.getName() + " for " + spellDamage + " hp.");
         } else {
             System.out.println("The caster missed!");
         }
     }
 
-    public Wizard(String name, double health, double maxHealth) {
+    public Wizard(String name, double health, double maxHealth, int spellOne, int spellTwo, int spellThree) {
         this.name = name;
         this.health = health;
         this.maxHealth = maxHealth;
+        switch (spellOne) {
+            case 1:
+                this.spellOne = fireBall;
+                break;
+            case 2:
+                this.spellOne = arrowRain;
+                break;
+            case 3:
+                this.spellOne = thunderbolt;
+                break;
+            case 4:
+                this.spellOne = bloodLeech;
+                break;
+            case 5:
+                this.spellOne = shadowWeapon;
+                break;
+        }
+        switch (spellTwo) {
+            case 1:
+                this.spellTwo = fireBall;
+                break;
+            case 2:
+                this.spellTwo = arrowRain;
+                break;
+            case 3:
+                this.spellTwo = thunderbolt;
+                break;
+            case 4:
+                this.spellTwo = bloodLeech;
+                break;
+            case 5:
+                this.spellTwo = shadowWeapon;
+                break;
+        }
+        switch (spellThree) {
+            case 1:
+                this.spellThree = fireBall;
+                break;
+            case 2:
+                this.spellThree = arrowRain;
+                break;
+            case 3:
+                this.spellThree = thunderbolt;
+                break;
+            case 4:
+                this.spellThree = bloodLeech;
+                break;
+            case 5:
+                this.spellThree = shadowWeapon;
+                break;
+        }
     }
+
+    public Wizard() {}
 
     public String getName() {
         return name;
     }
 
-    public String getSpells(){
+    public String getWizardSpells() {
+        return "1) " +this.spellOne.toString() + " \n2) "
+                + this.spellTwo.toString() + "\n3) "
+                + this.spellThree.toString() + "\n";
+    }
+
+    public String getAllSpells(){
         return fireBall.toString() + " \n "
                 + arrowRain.toString() + "\n"
                 + thunderbolt.toString() + "\n"
@@ -65,5 +129,59 @@ public class Wizard {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public void setWizardSpells(int spellOne, int spellTwo, int spellThree) {
+        switch (spellOne) {
+            case 1:
+                this.spellOne = fireBall;
+                break;
+            case 2:
+                this.spellOne = arrowRain;
+                break;
+            case 3:
+                this.spellOne = thunderbolt;
+                break;
+            case 4:
+                this.spellOne = bloodLeech;
+                break;
+            case 5:
+                this.spellOne = shadowWeapon;
+                break;
+        }
+        switch (spellTwo) {
+            case 1:
+                this.spellTwo = fireBall;
+                break;
+            case 2:
+                this.spellTwo = arrowRain;
+                break;
+            case 3:
+                this.spellTwo = thunderbolt;
+                break;
+            case 4:
+                this.spellTwo = bloodLeech;
+                break;
+            case 5:
+                this.spellTwo = shadowWeapon;
+                break;
+        }
+        switch (spellThree) {
+            case 1:
+                this.spellThree = fireBall;
+                break;
+            case 2:
+                this.spellThree = arrowRain;
+                break;
+            case 3:
+                this.spellThree = thunderbolt;
+                break;
+            case 4:
+                this.spellThree = bloodLeech;
+                break;
+            case 5:
+                this.spellThree = shadowWeapon;
+                break;
+        }
     }
 }
